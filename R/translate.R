0a45130cb64655ab6dffccd1d93413ad38b5b6f3
Joel Nitta <joelnitta@gmail.com>
2022-10-07 14:38:43 +0900

    WIP update translate script

--- R/translate.R
+++ R/translate.R
@@ -12,11 +12,10 @@ md2po(md_in = "index.qmd", po = "po/ja/index.po")
 md2po(md_in = "software.qmd", po = "po/ja/software.po")
 md2po(md_in = "blog.qmd", po = "po/ja/blog.po")
 md2po(md_in = "publications.qmd", po = "po/ja/publications.po")
+md2po(md_in = "posts/2022-10-07_canaper/index.qmd",
+  po = "po/ja/2022-10-07_canaper.po")
 
 # (edit PO files)
-
-# Switch to ja-source branch (source code for JA website)
-git_branch_checkout("ja-source")
 
 # Translate MD files **in place**
 po2md(md_in = "_quarto.yml", po = "po/ja/_quarto.po", md_out = "_quarto.yml")
@@ -24,12 +23,18 @@ po2md(md_in = "index.qmd", po = "po/ja/index.po", md_out = "index.qmd")
 po2md(md_in = "software.qmd", po = "po/ja/software.po", md_out = "software.qmd")
 po2md(md_in = "blog.qmd", po = "po/ja/blog.po", md_out = "blog.qmd")
 po2md(md_in = "publications.qmd", po = "po/ja/publications.po", md_out = "publications.qmd") # nolint
+po2md(md_in = "posts/2022-10-07_canaper/index.qmd",
+  po = "po/ja/2022-10-07_canaper.po",
+  md_out = "posts/2022-10-07_canaper/index.qmd") # nolint
 
 # Make any manual tweaks needed to translated docs
 
 # Optional: render translated webpage for local viewing
 quarto_preview()
 quarto_preview_stop()
+
+# Switch to ja-source branch (source code for JA website)
+git_branch_checkout("ja-source")
 
 # Commit changes to JA branch
 git_commit_all("Translate to JA")
