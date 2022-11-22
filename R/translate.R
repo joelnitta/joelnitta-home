a69255850e63cdcd319eb0ff0eee917d4696ce40
Joel Nitta <joelnitta@gmail.com>
2022-11-22 13:28:58 +0900

    Update translate script

--- R/translate.R
+++ R/translate.R
@@ -6,11 +6,17 @@ library(dovetail)
 library(quarto)
 library(gert)
 
-source("R/functions.R")
+# Create subdirectory for translation
+if (!fs::dir_exists("_locale/ja/")) {
+  fs::dir_create("_locale/ja")
+}
 
-# Create subdirectory populated with files to translate to JA
-# (will over-write any existing translation)
-site_create_locale("ja")
+# Recursively copy files needed to build translation to _locale/ja
+system(glue::glue(
+  "rsync -avu \\
+  --exclude '_locale*' --exclude '.git*' --exclude '_site' \\
+  --exclude 'renv' --exclude 'working' --exclude 'po' \\
+  ./ ./_locale/ja/"))
 
 # Create / update PO files
 md2po(md_in = "_quarto.yml", po = "po/ja/_quarto.po")
@@ -21,49 +27,60 @@ md2po(md_in = "publications.qmd", po = "po/ja/publications.po")
 md2po(md_in = "posts/2022-10-07_canaper/index.qmd",
   po = "po/ja/2022-10-07_canaper.po")
 
-# (edit PO files)
+# (edit PO files to use for translation)
 
-# Translate files
+# Translate source files
 po2md(
   md_in = "_quarto.yml",
   po = "po/ja/_quarto.po",
-  md_out = "locale/ja/_quarto.yml"
+  md_out = "_locale/ja/_quarto.yml"
 )
 po2md(
   md_in = "index.qmd",
   po = "po/ja/index.po",
-  md_out = "locale/ja/index.qmd"
+  md_out = "_locale/ja/index.qmd"
 )
 po2md(
   md_in = "software.qmd",
   po = "po/ja/software.po",
-  md_out = "locale/ja/software.qmd"
+  md_out = "_locale/ja/software.qmd"
 )
 po2md(
   md_in = "blog.qmd",
   po = "po/ja/blog.po",
-  md_out = "locale/ja/blog.qmd"
+  md_out = "_locale/ja/blog.qmd"
 )
 po2md(
   md_in = "publications.qmd",
   po = "po/ja/publications.po",
-  md_out = "locale/ja/publications.qmd"
-) # nolint
+  md_out = "_locale/ja/publications.qmd"
+)
 po2md(
   md_in = "posts/2022-10-07_canaper/index.qmd",
   po = "po/ja/2022-10-07_canaper.po",
-  md_out = "locale/ja/posts/2022-10-07_canaper/index.qmd"
+  md_out = "_locale/ja/posts/2022-10-07_canaper/index.qmd"
 )
 
-# Render any blog posts that have been translated
-quarto_render("locale/ja/posts/2022-10-07_canaper/index.qmd")
+# Render any blog posts that have been newly translated
+# (can skip those without new translations)
+quarto_render("_locale/ja/posts/2022-10-07_canaper/index.qmd")
 
 # Render translated webpage for local viewing
 # and make any manual tweaks needed to translated docs
-quarto_preview("locale/ja")
+# - need to manually edit for spaces:
+#    - software.qmd
+#    - publications.qmd
+quarto_preview("_locale/ja")
 quarto_preview_stop()
 
-# After verifying that site looks good, commit changes
-# (shouldn't need to add any new files;
-# all changes are translations of existing files)
-git_commit_all("Translate to JA")
+# After verifying that site looks good, commit changes to ja-source
+git_branch_checkout("ja-source")
+system(glue::glue(
+  "rsync -av \\
+  --exclude '_locale*' --exclude '.git*' --exclude '_site' \\
+  --exclude 'renv' --exclude 'working' --exclude 'po' \\
+  --exclude '*_cache' --exclude '.Rhistory' --exclude '.Rproj.user' \\
+  _locale/ja/ ./"
+))
+git_commit_all("Update JA translation")
+git_branch_checkout("main")
