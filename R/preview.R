da3dffc34dd529c1c6fba91619afc689fc458c17
Joel Nitta <joelnitta@gmail.com>
2023-09-27 10:54:24 +0900

    Add scripts to render site

---
+++ R/preview.R
@@ -0,0 +1,9 @@
+library(babelquarto)
+library(servr)
+
+# Rendering to view locally (preview)
+render_website()
+
+# View locally
+httw("_site")
+
---
+++ R/render.R
@@ -0,0 +1,5 @@
+library(babelquarto)
+library(servr)
+
+# For rendering to post online
+render_website(site_url = "https://www.joelnitta.com")
