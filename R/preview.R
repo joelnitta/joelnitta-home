3cca3d4b206be84604aa1170fdc4058d9bf164e5
joelnitta <joelnitta@gmail.com>
2023-10-06 15:55:45 +0900

    Don't need render script
    since now using GH actions

--- R/preview.R
+++ R/preview.R
@@ -1,6 +1,13 @@
 library(babelquarto)
 library(servr)
 
+# Optionally check URLs in references
+#TODO: expand this to all URLs in website
+check_urls <- FALSE
+if (check_urls) {
+  source("R/check_urls.R")
+}
+
 # Rendering to view locally (preview)
 render_website()
 
